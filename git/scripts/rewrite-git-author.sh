be1f0f7bcd891174727dfb96857e447ceec26249
mateusmed <mateus.medeiros46@gmail.com>
2026-04-02 18:04:49 -0300

    save scan by pattern

--- git/scripts/rewrite-git-author.sh
+++ git/scripts/rewrite-git-author.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
+# Configuração da nova identidade
 CORRECT_NAME="Mateus Medeiros"
 CORRECT_EMAIL="mateus.medeiros46@gmail.com"
 
 
-OLD_EMAIL_PATTERN="old.email@test.com"
+OLD_EMAIL_PATTERN="userA|userB\.com\.br|email\.com"
 
 if [ -z "$1" ]; then
     echo "Uso: $0 <caminho_do_repositorio>"
@@ -24,11 +24,11 @@ cd "$REPO_PATH" || exit 1
 
 # Comando para reescrever o histórico de todas as branches e tags
 git filter-branch -f --env-filter "
-if echo \"\$GIT_COMMITTER_EMAIL\" | grep -q \"$OLD_EMAIL_PATTERN\"; then
+if echo \"\$GIT_COMMITTER_EMAIL\" | grep -qiE \"$OLD_EMAIL_PATTERN\"; then
     export GIT_COMMITTER_NAME=\"$CORRECT_NAME\"
     export GIT_COMMITTER_EMAIL=\"$CORRECT_EMAIL\"
 fi
-if echo \"\$GIT_AUTHOR_EMAIL\" | grep -q \"$OLD_EMAIL_PATTERN\"; then
+if echo \"\$GIT_AUTHOR_EMAIL\" | grep -qiE \"$OLD_EMAIL_PATTERN\"; then
     export GIT_AUTHOR_NAME=\"$CORRECT_NAME\"
     export GIT_AUTHOR_EMAIL=\"$CORRECT_EMAIL\"
 fi
@@ -37,3 +37,5 @@ fi
 echo "--- Reescrita local concluída para $REPO_PATH ---"
 echo "Para atualizar o servidor remoto, entre na pasta e execute:"
 echo "git push origin --force --all && git push origin --force --tags"
+
+
